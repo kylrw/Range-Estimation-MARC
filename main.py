257bd050e9e2123dd4e4a0f729c708739e6f8d27
kylrw <kwitvoet03@gmail.com>
2023-01-07 00:04:10 -0500

    Working Model

--- main.py
+++ main.py
@@ -1,3 +1,93 @@
-#from scipy.io import loadmat
+#Import Libraries
+import math
+import numpy as np
+import pandas
+from scipy.io import loadmat
+import tensorflow as tf
+from keras.models import Sequential, Model
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 
-#annots = loadmat('TRAIN_LGHG2@n10degC_to_25degC_Norm_5Inputs.mat')
+# loads matlab files data into a python dict
+mat_data = loadmat('SOCprediction\TRAIN_LGHG2@n10degC_to_25degC_Norm_5Inputs.mat')
+
+# extracts the first 3 rows (Voltage, Current, Temp) and 100000 columns from the "X" key
+x_train = mat_data["X"][:3,:100000] 
+# extracts the first 100000 columns from the "Y" (SOC) key
+y_train = mat_data["Y"][:1,:100000] 
+
+x_train = pandas.DataFrame(x_train)
+
+# flips columns and rows so data is proper shape, have the same # of input features
+x_train = x_train.T
+y_train = y_train.T
+
+#Convert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train),np.array(y_train)
+#Reshape the data
+#x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+num_responses = 1
+
+num_features = 3
+
+num_hidden_units = 10
+
+epochs = 100 
+
+learn_rate_drop_period = 1000
+
+initial_learn_rate = 0.01
+
+learn_rate_drop_factor = 0.1
+
+training_data_len = 80000
+
+#Build the LSTM model
+# Define the model
+model = tf.keras.Sequential([
+  tf.keras.layers.InputLayer(input_shape=(num_features,), dtype=tf.float32, name="input"),
+  tf.keras.layers.BatchNormalization(center=True, scale=False, name="normalization"),
+  tf.keras.layers.Dense(num_hidden_units, activation=None, name="fc1"),
+  tf.keras.layers.Activation("tanh", name="tanh"),
+  tf.keras.layers.Dense(num_hidden_units, activation=None, name="fc2"),
+  tf.keras.layers.LeakyReLU(alpha=0.3, name="leaky_relu"),
+  tf.keras.layers.Dense(num_responses, activation=None, name="fc3"),
+  tf.keras.layers.ReLU(max_value=1, name="clipped_relu"),
+  tf.keras.layers.Dense(1, activation=None, name="output")
+])
+
+# Compile the model
+model.compile(
+  optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learn_rate),
+  loss=tf.keras.losses.MeanSquaredError(),
+  metrics=[tf.keras.metrics.MeanAbsoluteError()]
+)
+
+# Train the model
+model.fit(
+  x_train, y_train,
+  epochs=epochs,
+  batch_size=10000,
+  validation_data=(x_train, y_train),
+)
+
+x_test = x_train[training_data_len:, :]
+y_test = y_train[training_data_len:, :]
+
+y_pred = model.predict(x_test)
+
+plt.figure(figsize=(16,8))
+
+# Plot the predictions
+plt.plot(y_pred, label="Predictions")
+
+# Plot the true values
+plt.plot(y_test, label="True values")
+
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
