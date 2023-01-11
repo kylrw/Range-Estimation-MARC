b1f93d43ca6051fb00a27d94332f13a369f483af
kylrw <kwitvoet03@gmail.com>
2023-01-11 14:19:09 -0500

    new model to test on desktop

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ mat_data = loadmat('TRAIN_LGHG2@n10degC_to_25degC_Norm_5Inputs.mat')
 test_data = loadmat('04_TEST_LGHG2@25degC_Norm_(05_Inputs).mat')
 
 # Extracts the first 3 rows (Voltage, Current, Temp) and 100000 columns from the "X" key
-x_train = mat_data["X"][:3,:100000] 
+x_train = mat_data["X"][:3,:600000] 
 # Extracts the first 100000 columns from the "Y" (SOC) key
-y_train = mat_data["Y"][:1,:100000] 
+y_train = mat_data["Y"][:1,:600000] 
 
 #Create the x_test and y_test data sets
 x_test = test_data["X"][:3,:40000]
@@ -47,50 +47,39 @@ x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 # Using variables copied from MATLAB file
 
 num_responses = 1
-
 num_features = 3
-
 num_hidden_units = 10
-
 epochs = 100
-
-learn_rate_drop_period = 1000
-
-initial_learn_rate = 0.01
-
-learn_rate_drop_factor = 0.01
-
-batch_size = 10000
-
-training_data_len = 80000
-
-timesteps = 3
-
-data_dim = 1
+batch_size = x_test.shape[0]
+learn_rate_drop_period = 2000
+LearningRate = 0.01
+learn_rate_drop_factor = 0.5
 
 # Build the LSTM model
 # Defined the model using same design as the Abstract
 model = Sequential()
-model.add(tf.keras.layers.Input(shape=(timesteps, data_dim),name="input"))
-model.add(LSTM(num_hidden_units,name="LSTM"))
-model.add(Dense(num_hidden_units,name="FullyConnectedLayer"))
-model.add(tf.keras.layers.ReLU(max_value=1,name="ClippedRELU"))
-model.add(Dense(1,name="output"))
+model.add(LSTM(10, batch_input_shape=(batch_size, x_train.shape[1],1), stateful=True,return_sequences=False))
+model.add(Dense(10))
+model.add(tf.keras.layers.ReLU(max_value=1))
+model.add(Dense(1))
+model.summary()
 
-# Compile the model, following MATLAB file again
-model.compile(
-  optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learn_rate),
-  loss=tf.keras.losses.MeanSquaredError(),
-  metrics=[tf.keras.metrics.MeanSquaredError()]
-)
+#Define the learning rate scheduler
+def scheduler(epoch, lr):
+    if epoch % learn_rate_drop_period == 0 and epoch:
+        return lr * learn_rate_drop_factor
+    else:
+        return lr
 
-# Train the model
-model.fit(
-  x_train, y_train,
-  epochs=epochs,
-  batch_size=batch_size,
-  validation_data=(x_train, y_train),
-)
+#Define the learning rate scheduler callback
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+#Compile the model with a learning rate scheduler
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LearningRate), loss='mean_squared_error')
+
+#Train the model
+model.fit(x_train,y_train, batch_size=batch_size, epochs=epochs, callbacks=[lr_scheduler],validation_data=(x_test, y_test),)
+# added validation data
 
 # Predicts SOC data using the trained LSTM model
 #make predictions Stateful = True
