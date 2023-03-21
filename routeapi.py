c9b36307117c2330f63f99b591f9508126b83abb
Kyler Witvoet <kwitvoet03@gmail.com>
2023-03-21 16:48:55 -0400

    update

--- routeapi.py
+++ routeapi.py
@@ -8,39 +8,53 @@ This program will use 4 google maps API,s to get:
 
 """
 
-import urllib.request
 import os
 import requests
-import json
-import time
-import datetime
-
+import googlemaps
 import login_creds
+from datetime import datetime
 
 api_key = os.getenv('api_key')
+gmaps = googlemaps.Client(key=api_key)
+
 
 # Take source as input
-source = "Ancaster"
+source = "57 Joanne Crt, Ancaster ON Canada"
 #input("Enter source: ")
   
+
 # Take destination as input
-dest = "NiagaraFalls"
-#input("Enter destination:")
-  
-# url variable store url 
-url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
-  
-# Get method of requests module
-# return response object
-r = requests.get(url + 'origins=' + source +
-                   '&destinations=' + dest +
-                   '&key=' + api_key)
-                     
-# json method of response object
-# return json format result
-x = r.json()
-  
-# by default driving mode considered
-  
-# print the value of x
-print(x)
+dest = "145 Bowmen St, Hamilton ON Canada"
+
+
+# Request directions via driving
+now = datetime.now()
+directions_result = gmaps.directions(source,
+                                     dest,
+                                     mode="driving",
+                                     departure_time=now)
+
+
+# print the directiions step by step
+#for i in range(len(directions_result[0]['legs'][0]['steps'])):
+    #for direction in directions_result[0]['legs'][0]['steps'][i]['html_instructions']:
+       # print(direction, end="")
+   # print()
+
+# print the distance between the two points
+distance = directions_result[0]['legs'][0]['distance']['text']
+
+# appends the start and end locations of each step to a list
+locations = []
+for i in range(len(directions_result[0]['legs'][0]['steps'])):
+    locations.append([directions_result[0]['legs'][0]['steps'][i]['start_location']['lat'],directions_result[0]['legs'][0]['steps'][i]['end_location']['lng']])
+
+# Request elevation data
+elevations = gmaps.elevation(locations)
+
+# Request the average speed of every step in locations
+speeds = gmaps.snapped_speed_limits(locations)
+
+# print the average speed of every step
+print(speeds)
+
