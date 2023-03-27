5dd80a40abfde9855152b219546e1998c166442f
Kyler Witvoet <kwitvoet03@gmail.com>
2023-03-26 22:22:39 -0400

    range estimation! V1

---
+++ Helper Programs/xlsx_dataextract_SOC.py
@@ -0,0 +1,62 @@
+"""
+This program will read all xlsx files, search all sheets for the columns;
+
+    - SOC
+    - Voltage
+    - Current
+    - Temperature
+
+When a columns has been found, it will be added to the respective list.
+If a column is not found any where in the file, the file will be skipped, and a message will be printed to the console.
+If all columns are successfully found, they will be added to a csv file, until the csv file has >= 100000 rows.
+"""
+
+import pandas as pd
+import os
+import csv
+
+# Get all the excel files in the current directory
+excel_files = [f for f in os.listdir('Data/philcar') if f.endswith('.xlsx')]
+
+with open('reformated3.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['SOC', 'V', 'I', 'T'])
+
+    # Loop through all the excel files
+    for excel_file in excel_files:
+        # Read the excel file into a pandas dataframe
+        df = pd.read_excel("Data/philcar/"+excel_file, sheet_name=None)
+
+        soc = []
+        v = []
+        i = []
+        t = []
+
+        # gather the values from the dataframe if they are not NaN
+        for sheet in df:
+            if 'HV Battery SOC [%]' in df[sheet].columns:
+                soc = df[sheet]['HV Battery SOC [%]']
+            if 'HV Battery Voltage [V]' in df[sheet].columns:
+                v = df[sheet]['HV Battery Voltage [V]']
+            if 'HV Battery Current [A]' in df[sheet].columns:
+                i = df[sheet]['HV Battery Current [A]']
+            if 'OAT [degC]' in df[sheet].columns:
+                t = df[sheet]['OAT [degC]']
+
+        if soc.empty or v.empty or i.empty or t.empty:
+            print('Error: NaN value in file ' + excel_file)
+            continue
+        else:
+            print('Found all columns in file ' + excel_file)
+            
+            # iterate through the lists and write the data to the csv file
+            for x in range(len(soc)):
+                # double check that the data is not NaN
+                if soc[x] == soc[x] and v[x] == v[x] and i[x] == i[x] and t[x] == t[x]:
+                    writer.writerow([soc[x], v[x], i[x], t[x]])
+    
+        # Check if the csv file has 100000 rows
+        if len(open('reformated3.csv').readlines()) >= 200000:
+            print('Done!')
+            break
+            
---
+++ Helper Programs/xlsx_dataextract_range.py
@@ -0,0 +1,132 @@
+"""
+This program will go through every excel file in the folder Data/philcar it will parse the data as specified and write it to a csv file.
+
+Parsing Specifications:
+The program will gather the following information from the second sheet in each excel file:
+    - Initial SOC
+    - Final SOC
+    - Initial Altitude
+    - Final Altitude
+    - Average Speed
+    - Final Accumulated Distance
+
+To gather this data the program will break up each excel file into chunks between 10 and 100 minutes long (600 - 6000 rows). 
+These chunks can overlap with each other by half the chunk size.
+
+The program will then calculate the following data before writing it to a csv file:
+    - Initial SOC (First value of SOC in the chunk)
+    - Final SOC (Last value of SOC in the chunk)
+    - Altitude Difference (Final Altitude - Initial Altitude)
+    - Average Speed (Average of all the speeds in the chunk)
+    - Final Accumulated Distance (Final Distance - Initial Distance)
+
+"""
+
+import os
+import csv
+import pandas as pd
+import numpy as np
+
+
+def get_chunks(data, chunk_size, overlap) -> list:
+    """This function will break up the data into chunks of chunk_size long, with overlap between the chunks.
+
+    Args:
+        data (list): The data to be broken up into chunks.
+        chunk_size (int): The size of each chunk.
+        overlap (int): The amount of overlap between each chunk.
+
+    Returns:
+        list: A list of lists, where each list is a chunk of data.
+    """
+    chunks = []
+    for x in range(0, len(data), chunk_size - overlap):
+        if x + chunk_size < len(data):
+            chunks.append(data[x:x + chunk_size])
+    return chunks   
+
+
+
+def process_chunk(chunk) -> list:
+    """This function will process a chunk of data.
+
+    Args:
+        chunk (list): A list of data.
+
+    Returns:
+        list: A list of the processed data.
+    """
+    # Get the data from the chunk
+    soc = chunk['HV Battery SOC [%]']
+    altitude = chunk['Altitude_filt [m]']
+    speed = chunk['Vehicle Speed [m/s]']
+    distance = chunk['Accumulated_Distance [m]']
+
+    # Calculate the data
+    initial_soc = soc.iloc[0]
+    final_soc = soc.iloc[-1]
+    altitude_difference = altitude.iloc[-1] - altitude.iloc[0]
+    average_speed = np.mean(speed)
+    final_accumulated_distance = distance.iloc[-1] - distance.iloc[0]
+
+    # Return the data
+    return [initial_soc, final_soc, altitude_difference, average_speed, final_accumulated_distance]
+
+def main():
+
+    # Get all the excel files in the current directory
+    excel_files = [f for f in os.listdir('Data/philcar') if f.endswith('.xlsx')]
+
+    # Create a new csv file to write to
+    with open('Data/phil_rangedata_train.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['Initial SOC', 'Final SOC', 'Altitude Difference', 'Average Speed', 'Final Accumulated Distance'])
+
+        # Loop through all the excel files
+        for excel_file in excel_files:
+            # Read the excel file into a pandas dataframe
+            df = pd.read_excel('Data/philcar/' + excel_file, sheet_name=1)
+
+            previous_row = []
+
+            # Get the chunks of data for 10 mins, 30 mins, 60 mins, 90 mins, and 120 mins
+            for chunk_size in [600, 1800, 3600, 5400, 7200]:
+                overlap = int(chunk_size/2)
+
+                if chunk_size > len(df):
+                    continue
+
+                chunks = get_chunks(df, chunk_size, overlap)
+
+                # print the current status
+                print(f'Processing {excel_file} with chunk size {chunk_size} and overlap {overlap}')
+
+                # Loop through all the chunks
+                for chunk in chunks:
+                    # Process the chunk
+                    processed_chunk = process_chunk(chunk)
+
+                    # check if all the values in the chunk are not NaN and the final accumulated distance is not the same as the row above it in the csv file
+                    if not np.isnan(processed_chunk).any() and processed_chunk != previous_row:
+                        # Write the processed chunk to the csv file
+                        writer.writerow(processed_chunk)
+                        previous_row = processed_chunk
+            
+            # gathers the data for the entire file
+            processed_chunk = process_chunk(df)
+            if not np.isnan(processed_chunk).any():
+                writer.writerow(processed_chunk)
+                #print status
+                print(excel_file, 'Done')
+                            
+if __name__ == '__main__':
+    main()
+
+    
+    #randomly reformat the rows in the csv file
+    df = pd.read_csv('Data/phil_rangedata_train.csv')
+    df = df.sample(frac=1).reset_index(drop=True)
+    df.to_csv('Data/phil_rangedata_train.csv', index=False)
+
+    print('Done')
+    
