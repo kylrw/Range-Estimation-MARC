1818fde42d33c7fc5dd375c1d08f8a14d36ab1ce
kylrw <kwitvoet03@gmail.com>
2023-03-29 12:58:04 -0400

    server change

--- server_autogluon_SOC.py
+++ server_autogluon_SOC.py
@@ -1,10 +1,7 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
+import autogluon.core as ag
 import numpy as np
 import math
-import os
-import time
-
-print(os.getcwd())
 
 train_data = TabularDataset(f'Data/phil_socdata_train.csv')
 train_data.head()
@@ -12,7 +9,38 @@ train_data.head()
 label = 'SOC'
 train_data[label].describe()
 
-predictor = TabularPredictor(label=label,eval_metric='root_mean_squared_error').fit(train_data, presets='best_quality',time_limit=15*60*60,ag_args_fit={'num_gpus':4})
+nn_options = {  # specifies non-default hyperparameter values for neural network models
+    'num_epochs': 10,  # number of training epochs (controls training time of NN models)
+    'learning_rate': ag.space.Real(1e-4, 1e-2, default=5e-4, log=True),  # learning rate used in training (real-valued hyperparameter searched on log-scale)
+    'activation': ag.space.Categorical('relu', 'softrelu', 'tanh'),  # activation function used in NN (categorical hyperparameter, default = first entry)
+    'dropout_prob': ag.space.Real(0.0, 0.5, default=0.1),  # dropout probability (real-valued hyperparameter)
+}
+
+gbm_options = {  # specifies non-default hyperparameter values for lightGBM gradient boosted trees
+    'num_boost_round': 100,  # number of boosting rounds (controls training time of GBM models)
+    'num_leaves': ag.space.Int(lower=26, upper=66, default=36),  # number of leaves in trees (integer hyperparameter)
+}
+
+hyperparameters = {  # hyperparameters of each model type
+                   'GBM': gbm_options,
+                   'NN_TORCH': nn_options,  # NOTE: comment this line out if you get errors on Mac OSX
+                  }  # When these keys are missing from hyperparameters dict, no models of that type are trained
+
+time_limit = 30*60  # train various models for ~10 min
+num_trials = 50  # try at most 5 different hyperparameter configurations for each type of model
+search_strategy = 'auto'  # to tune hyperparameters using random search routine with a local scheduler
+
+hyperparameter_tune_kwargs = {  # HPO is not performed unless hyperparameter_tune_kwargs is specified
+    'num_trials': num_trials,
+    'scheduler' : 'local',
+    'searcher': search_strategy,
+}
+
+predictor = TabularPredictor(label=label, eval_metric='root_mean_squared_error').fit(
+    train_data, time_limit=time_limit, auto_stack=True, presets='best_quality',
+    hyperparameters=hyperparameters, hyperparameter_tune_kwargs=hyperparameter_tune_kwargs,
+)
+
 
 test_data = TabularDataset(f'Data/phil_socdata_test.csv')
 
@@ -31,4 +59,3 @@ plt.savefig("Pictures/soc_predictions.png")
 mse_test = np.mean(((y_pred - test_data[label])**2))
 rmse_test = math.sqrt(mse_test)
 print("test data rmse:", rmse_test)
-print(time.strftime("%I:%M:%S %p"))
