956756b22bdd4d77cbb6ec9868266b9418539655
Kyler Witvoet <kwitvoet03@gmail.com>
2023-04-11 19:10:49 +0000

    added rolling averages to data

--- xlsx_dataextract_SOC.py
+++ xlsx_dataextract_SOC.py
@@ -19,18 +19,18 @@ from scipy.io import loadmat
 # Get all the excel files in the current directory
 excel_files = [f for f in os.listdir('Data/philcar') if f.endswith('.xlsx')]
 
-filename = 'Data/phil_socdata_trainLSTM.csv'
+filename = 'Data/phil_socdata_train.csv'
 
 # Open the csv file and write the column names
 with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(['item_id','timestep','SOC', 'V', 'I', 'T','V_avg','I_avg'])
+    writer.writerow(['SOC', 'V', 'I', 'T','P','V_avg','I_avg'])
 
     # Loop through all the excel files
     for excel_file in excel_files:
 
         # skip the excel file with name Phil_DC_93_10degC.xlsx as it will be used as test data
-        if excel_file == 'Phil_DC_93_10degC.xlsx' or excel_file == 'Phil_DC_86_10degC.xlsx':
+        if excel_file == 'Phil_DC_93_10degC.xlsx' or excel_file == 'Phil_DC_27_16degC.xlsx':
             continue
 
         # Read the excel file into a pandas dataframe
@@ -43,6 +43,7 @@ with open(filename, 'w', newline='') as csvfile:
         t = []
         v_avg = []
         i_avg = []
+        p = []
 
         # gather the values from the dataframe if they are not NaN
         for sheet in df:
@@ -71,33 +72,13 @@ with open(filename, 'w', newline='') as csvfile:
                     timestep = pd.to_datetime(timestep, unit='s')
                     #converts the timestamp into a string
                     timestep = timestep.strftime('%Y-%m-%d %H:%M:%S')
-                    # v_avg is the average voltage of all previous values including the current value
-                    v_avg.append(sum(v[:x+1])/(x+1))
-                    # i_avg is the average current of all previous values including the current value
-                    i_avg.append(sum(i[:x+1])/(x+1))
+                    # v_avg is the average voltage of the 500 previous values including the current value
+                    v_avg.append(sum(v[max(0, x-499):x+1])/min(500, x+1))
+
+                    # i_avg is the average current of the 500 previous values including the current value
+                    i_avg.append(sum(i[max(0, x-499):x+1])/min(500, x+1))
+
                     #writes the data to the csv file
-                    writer.writerow(['SOC',timestep,soc[x], v[x], i[x], t[x], v_avg[x], i_avg[x]])
+                    writer.writerow([soc[x], v[x], i[x], t[x], v[x]*i[x],v_avg[x], i_avg[x]])
 
-'''
-# open all mat files in Data/New MATLAB Data and add their columns to the csv file in the same way as the excel files
-mat_files = [f for f in os.listdir('Data/New MATLAB Data') if f.endswith('.mat')]
-with open(filename, 'a', newline='') as csvfile:
-
-    writer = csv.writer(csvfile, delimiter=',')
-    for mat_file in mat_files:
-        mat = loadmat("Data/New MATLAB Data/"+mat_file)
-        soc = mat['Y'].T[:,0]
-        v = mat['X'].T[:,0]
-        i = mat['X'].T[:,1]
-        t = mat['X'].T[:,2]
-        v_avg = []
-        i_avg = []
-        for x in range(len(soc)):
-            if soc[x] == soc[x] and v[x] == v[x] and i[x] == i[x] and t[x] == t[x]:
-                v_avg.append(sum(v[:x+1])/(x+1))
-                i_avg.append(sum(i[:x+1])/(x+1))
-                writer.writerow([x,soc[x], v[x], i[x], t[x], v_avg[x], i_avg[x]])
-        print( str(mat_files.index(mat_file)) + 'Found all columns in file:' + mat_file)
-'''
-    
 print('Done!')
