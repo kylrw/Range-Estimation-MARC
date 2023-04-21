e3e6fe5e9aeb1755106dc526cc1b2d32b61fe6d4
Kyler Witvoet <kwitvoet03@gmail.com>
2023-04-21 05:11:55 +0000

    new range csv

--- Helper Programs/xlsx_dataextract_range.py
+++ Helper Programs/xlsx_dataextract_range.py
@@ -78,7 +78,7 @@ def main():
     excel_files = [f for f in os.listdir('Data/philcar') if f.endswith('.xlsx')]
 
     # Create a new csv file to write to
-    with open('Data/phil_rangedata_test2.csv', 'w', newline='') as csvfile:
+    with open('Data/phil_rangedata_train.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['initial_SOC', 'final_SOC', 'altitude', 'avg_speed', 'total_distance'])
 
@@ -86,7 +86,7 @@ def main():
         for excel_file in excel_files:
 
             # skip the excel file with name Phil_DC_93_10degC.xlsx as it will be used as test data
-            if excel_file == 'Phil_DC_93_10degC.xlsx' or excel_file != 'Phil_DC_86_10degC.xlsx':
+            if excel_file == 'Phil_DC_93_10degC.xlsx' or excel_file == 'Phil_DC_86_10degC.xlsx':
                 continue
 
             # Read the excel file into a pandas dataframe
@@ -95,7 +95,7 @@ def main():
             previous_row = []
 
             # Get the chunks of data for 10 mins, 30 mins, 60 mins, 90 mins, 120 mins, and 150 mins and the entire chunk
-            for chunk_size in [600, 1800, 3600, 5400, 7200, 9000]:
+            for chunk_size in [600, 1800, 3600, 5400, 7200, 9000, len(df)-1]:
                 overlap = int(chunk_size / 2)
 
                 if chunk_size > len(df):
@@ -107,13 +107,13 @@ def main():
                 print(f'Processing {excel_file} with chunk size {chunk_size} and overlap {overlap}')
 
                 # Loop through all the chunks
-                for chunk in chunks:
+                for i in range(1):
                     # Process the chunk
-                    processed_chunk = process_chunk(chunk)
+                    processed_chunk = process_chunk(chunks[i])
 
                     # check if all the values in the chunk are not NaN and the final accumulated distance is not the same as the row above it in the csv file
                     if not np.isnan(processed_chunk).any() and processed_chunk != previous_row:
-                        # Write the processed chunk to the csv file
+                        # Write the only the first row of the processed chunk to the csv file
                         writer.writerow(processed_chunk)
                         previous_row = processed_chunk
 
